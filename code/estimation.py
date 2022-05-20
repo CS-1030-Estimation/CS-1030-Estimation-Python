b13bf5d786d45d13816294f6156be2423e6677b3
unknown <siggardskylar@gmail.com>
2022-05-19 22:08:43 -0600

    tying to get it to a dict that is easier to work with, but failed

--- code/estimation.py
+++ code/estimation.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv(r'D:\Coding\UVU Classes\CS1030\Projects\CS-1030-Estimation-Python\data\onscreen_takeoff.csv').to_dict()
+try:
+    df = pd.read_csv(r'D:\Coding\UVU Classes\CS1030\Projects\CS-1030-Estimation-Python\data\onscreen_takeoff.csv').to_dict()
+    try:
+        del df['Not Needed1']
+        del df['Not Needed2']
+        del df['Not Needed3']
+        del df['Not Needed4']
+    except KeyError:
+        print("Could not remove unneeded Items")
+except KeyError:
+    print("No CSV file Found")
 
-
-print(df)
-
-
+#print(type(df), df)
+for i in df['Building'].items():
+    print(i)
