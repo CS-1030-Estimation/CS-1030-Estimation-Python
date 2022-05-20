f8c7eb58f6a4b6c6de1aacdcfdaf0e94a4adba4e
unknown <siggardskylar@gmail.com>
2022-05-19 21:04:23 -0600

    done

--- code/estimation.py
+++ code/estimation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv(r'D:\Coding\UVU Classes\CS1030\Projects\CS-1030-Estimation-Python\data\onscreen_takeoff.csv')
+df = pd.read_csv(r'D:\Coding\UVU Classes\CS1030\Projects\CS-1030-Estimation-Python\data\onscreen_takeoff.csv').to_dict()
 
-for column in df:
-    print(column)
+
+print(df)
 
 
